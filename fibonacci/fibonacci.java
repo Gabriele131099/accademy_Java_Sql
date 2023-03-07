e65b9ba27df3604e09d7771484b7ea207bc3a45c
Gabriele131099 <81107856+Gabriele131099@users.noreply.github.com>
2023-03-07 14:01:21 +0100

    integrazioni

--- fibonacci/fibonacci.java
+++ fibonacci/fibonacci.java
@@ -11,15 +11,10 @@ public class fibonacci {
     static ArrayList<Integer> numsGenerated = new ArrayList<Integer>();
 
     public static void main(String[] args) {
-        connection();
-        // ------------------------------------------------------------------
-
         boolean keep = true;
         while (keep) {
-            Scanner choiceScanner = new Scanner(System.in);
-            System.out.println("Vuoi\n[1] Stampare la serie\n[2] Verificare un numero\n[3] Uscire");
-            int userChoice = choiceScanner.nextInt();
-
+            int userChoice = requestInput(
+                    "Vuoi\n[1] Stampare la serie\n[2] Verificare un numero\n[3] Prova connessione\n[4] Uscire", 4);
             switch (userChoice) {
                 case 1:
                     printSequence();
@@ -29,25 +24,51 @@ public class fibonacci {
                     searchNumberInSequence();
                     break;
                 case 3:
-                    System.out.println("Uscita...");
+                    connection();
+                    break;
+                case 4:
+
+                    System.out.println("Esci");
                     keep = false;
                     break;
                 default:
-                    System.out.println("Opzione non disponibile");
+                    System.out.println("non disponibile");
             }
 
         }
 
     }
-
+//con limite
+    public static int requestInput(String request, int choiseMax) {
+        Boolean flagChoise = false;
+        int inputNumber = 0;
+        do {
+            Scanner nScan = new Scanner(System.in);
+            System.out.println(request);
+            inputNumber = nScan.nextInt();
+            flagChoise = (inputNumber >= 0 && inputNumber <= choiseMax);
+        } while (!flagChoise);
+        return inputNumber;
+    }
+//generic int input
+    public static int requestInput(String request) {
+        Boolean flagChoise = false;
+        int inputNumber = 0;
+        do {
+            Scanner nScan = new Scanner(System.in);
+            System.out.println(request);
+            inputNumber = nScan.nextInt();
+            flagChoise = inputNumber >= 0;
+        } while (!flagChoise);
+        return inputNumber;
+    }
+//stampa
     public static void printSequence() {
         int num1 = 0;
         int num2 = 1;
         int num3;
 
-        Scanner inputScanner = new Scanner(System.in);
-        System.out.println("Quante volte vuoi stampare la serie?");
-        int userInput = inputScanner.nextInt();
+        int userInput = requestInput("Quale serie vuoi stampare?");
         // stampo 0 e 1 che sono sempre i primi due numeri della serie
         System.out.println(num1);
         System.out.println(num2);
@@ -59,11 +80,10 @@ public class fibonacci {
             num2 = num3;
         }
     }
-
+//cerca numero
     public static void searchNumberInSequence() {
-        Scanner numScanner = new Scanner(System.in);
-        System.out.println("Inserisci il numero che vuoi ricercare");
-        int numToCheck = numScanner.nextInt();
+
+        int numSearch = requestInput("Inserisci il numero che vuoi cercare nella serie");
         int index = 0;
         int n1 = 0;
         int n2 = 1;
@@ -76,14 +96,14 @@ public class fibonacci {
             numsGenerated.add(n3);
             index++;
         }
-        // controllo se il numero fa parte della serie
-        if (numsGenerated.contains(numToCheck)) {
-            System.out.println("Il numero " + numToCheck + " fa parte della serie");
+        // controlla se il numero fa parte della serie
+        if (numsGenerated.contains(numSearch)) {
+            System.out.println("Il numero " + numSearch + " fa parte della serie");
         } else {
-            System.out.println("Il numero " + numToCheck + " non fa parte della serie");
+            System.out.println("Il numero " + numSearch + " non fa parte della serie");
         }
     }
-
+//connessione 
     public static void connection() {
         // Connessione database --------------------------------------------
         String DB_URL = "jdbc:mysql://localhost:3306/world";
@@ -96,42 +116,81 @@ public class fibonacci {
             Class.forName(DB_DRIVER);
             conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
             if (conn != null) {
-                System.out.println("Connessione eseguita con successo");
+                System.out.println("Connessione eseguita");
 
             } else {
                 System.out.println("Connessione fallita");
             }
             Statement stm = conn.createStatement();
-
-            ResultSet rs = stm.executeQuery(query3());
+            int choiseQuery = requestInput("Scegli una query casualmente, inserisci un numero fra 0 e 4", 4);
+            String query = returnChoiseQuery(choiseQuery);
+            ResultSet rs = stm.executeQuery(query);
             while (rs.next()) {
                 String stringa = String.format("ID: %d; Name: %s;  ",
                         rs.getInt(1),
-                        rs.getString(3));
+                        rs.getString(2));
                 System.out.println(stringa);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+//sceglie la  query
+    public static String returnChoiseQuery(int choise) {
+        String s = "";
+        switch (choise) {
+            case 0:
+                query1();
+                break;
+            case 1:
+                query2();
+                break;
+            case 2:
+                query3();
+                break;
+            case 3:
+                query4();
+                break;
+            case 4:
+                query5();
+                break;
+            default:
+                break;
+        }
 
+        return s;
+    }
+//varie query
     public static String query1() {
         // Lingua e Nazione di Ogni Città
+        System.out.println("Lingua e Nazione di Ogni Città");
         return "SELECT co.code, c.name, co.name, cl.language, cl.percentage FROM city as c INNER JOIN country as co ON c.CountryCode = co.code INNER JOIN countrylanguage as cl on co.code = cl.countryCode order by c.name ASC;";
     }
 
     public static String query2() {
         // Numero di Città per Nazione
+        System.out.println("Numero di Città per Nazione");
         return "SELECT co.Code, co.name, COUNT(city.name) FROM country as co INNER JOIN city ON co.Code = city.CountryCode GROUP BY co.name ORDER BY COUNT(city.name);";
     }
 
     public static String query3() {
         // Repubbliche con aspettativa di vita maggiore 70 e lingua
+        System.out.println("Repubbliche con aspettativa di vita maggiore 70 e lingua");
         return "SELECT co.Code, co.name, co.GovernmentForm, cl.Language, co.LifeExpectancy FROM country as co inner join countrylanguage as cl on co.Code = cl.CountryCode where co.LifeExpectancy>70 AND co.GovernmentForm like 'Republic' and cl.IsOfficial='T' ORDER BY co.LifeExpectancy desc";
     }
 
     public static String query4() {
         // 5 cita da city
+        System.out.println("5 cita da city");
         return "select * from city Limit 5";
+    }
+
+    public static String query5() {
+        // Creare una view di city del database world su una query che mostra le città
+        // italiane. Su questa VIEW eseguire una query che mostra solo le città con una
+        // popolazione inferiore ai 100k.
+        System.out.println(
+                "Creare una view di city del database world su una query che mostra le città italiane. Su questa VIEW eseguire una query che mostra solo le città con una popolazione inferiore ai 100k.");
+        return "Create View cityView Select city.name,city popolazione From world Where city.name=='Italia'  AND city.popolazione >100k ";
     }
 }
