a9bfd539d1bf75b439b311c253bf8d1b938c27a1
Gabriele131099 <81107856+Gabriele131099@users.noreply.github.com>
2023-03-07 16:55:28 +0100

    aggiunta interfaccia utente

--- es java/07-03-23/concessionaria/Main.java
+++ es java/07-03-23/concessionaria/Main.java
@@ -1,23 +1,95 @@
 package concessionaria;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // array moto e macchina
+        ArrayList<Auto> arrayAuto = new ArrayList<>();
+        ArrayList<Moto> arrayMoto = new ArrayList<>();
         // Macchine Auto
-        Auto mac1 = new Auto("Ford", "Focus", 2000, 3, 
-            "Disel", 26);
-        Auto mac2 = new Auto("Opel", "Corsa", 2006, 5, 
+        Auto mac1 = new Auto("Ford", "Focus", 2000, 3,
+                "Disel", 26);
+        Auto mac2 = new Auto("Opel", "Corsa", 2006, 5,
                 "Benzina", 23);
-
-        mac1.stampaMacchina();
-        mac2.stampaMacchina();
+        arrayAuto.add(mac1);
+        arrayAuto.add(mac2);
 
         // Moto
         Moto moto1 = new Moto("Aprilia", "X12", 2008, 400,
                 "Sportiva", 200);
         Moto moto2 = new Moto("BMW", "Y2", 2015, 600,
                 "Strada", 500);
+        arrayMoto.add(moto1);
+        arrayMoto.add(moto2);
 
-        moto1.stampaMoto();
-        moto2.stampaMoto();
+        Boolean flagChoiseUser = false;
+        String allTypeMoto ="Scegli il tipo di moto: ";
+        String[] typeMoto  = {"sportiva","strada","cross"};
+        for (int i = 0; i < typeMoto.length; i++) {
+            allTypeMoto+=(i==0?" ":", ")+i+" "+typeMoto[i];
+        }
+        do {
+            int choiseUser = requestInput("Vuoi creae un Auto o una moto? 0 o 1, 2 esci", 3);
+            flagChoiseUser = choiseUser < 2;
+            if (choiseUser == 0) {
+                System.out.println("Crea una macchina");
+                Auto mac = new Auto(requestInputString("Marca"), requestInputString("Modello"), requestInputInt("Anno Fabbricazione"), requestInputInt("Numero Porte"),
+                requestInputString("Tipo Carburante"), requestInputInt("Litri per 100km"));
+                arrayAuto.add(mac);
+                System.out.println("Hai creato un Auto");
+                mac.stampaMacchina();
+            }
+            if (choiseUser == 1) {
+                System.out.println("Crea una moto");
+                Moto moto = new Moto(requestInputString("Marca"), requestInputString("Modello"), requestInputInt("Anno Fabbricazione"), requestInputInt("Cilindrata"),
+                        typeMoto[requestInput(allTypeMoto,typeMoto.length-1)], requestInputInt("Potenza"));
+
+                arrayMoto.add(moto);
+                System.out.println("Hai creato una moto");
+                moto.stampaMoto();
+            }
+            if (choiseUser > 1) {
+                System.out.println("Ciao amico");
+            }
+        } while (flagChoiseUser);
+
+        System.out.println("Tutte le nostre Moto");
+        for (Moto moto : arrayMoto) {
+            moto.stampaMoto();
+        }
+
+        System.out.println("Tutte le nostre Auto");
+        for (Auto mac : arrayAuto) {
+            mac.stampaMacchina();
+        }
+        
+    }
+
+    public static int requestInput(String request, int choiseMax) {
+        Scanner nScan = new Scanner(System.in);
+        Boolean flagChoise = false;
+        int inputNumber = 0;
+        do {
+            System.out.println(request);
+            inputNumber = nScan.nextInt();
+            flagChoise = (inputNumber >= 0 && inputNumber <= choiseMax);
+        } while (!flagChoise);
+        return inputNumber;
+    }
+    public static int requestInputInt(String request) {
+        Scanner nScan = new Scanner(System.in);
+        int inputNumber = 0;
+            System.out.println(request);
+            inputNumber = nScan.nextInt();
+        return inputNumber;
+    }
+    public static String requestInputString(String request) {
+        Scanner sScan = new Scanner(System.in);
+        String inputString;
+            System.out.println(request);
+            inputString = sScan.nextLine();
+        return inputString;
     }
 }
